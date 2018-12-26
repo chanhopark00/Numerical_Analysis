7de25012996e228c843d47e5269ae13e6882064f
Chan Ho Park <36185920+chanhopark00@users.noreply.github.com>
2018-12-26 20:36:46 +0800

    Added Pyplot

--- trapezoidal_and_simpson_rule.py
+++ trapezoidal_and_simpson_rule.py
@@ -1,8 +1,8 @@
 # function is e^x * cos(x)
-
 from numpy import poly1d
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 def f(x):
     return math.exp(x)* np.cos(x)
@@ -18,14 +18,19 @@ def trapezoid(k, n):
         return trapezoid(k+1,n) + (f(x(k+1,n)) + f(x(k,n)))* np.pi / n / 2
 def x(k,n):
     return np.pi * k / n
-def simpson_err(n):
-    print("Simpson error when n =",n,":",simpson(0,n) + 12.0703463164)
-def trapezoid_err(n):
-    print("Trapezoidal error when n =",n,":",trapezoid(0,n) + 12.0703463164)
+def simpson_err(n,err):
+    err = err + [simpson(0,n) + 12.0703463164]
+def trapezoid_err(n,err):
+    err = err + [trapezoid(0,n) + 12.0703463164]
 
 a = [2,4,8,16,32,64,128]
+err = []
 for i in a:
-    simpson_err(i)
+    simpson_err(i,err)
+plt.plot(a,err)
+plt.ylabel("Error of simpson rule")
 for i in a:
-    trapezoid_err(i)
-    
+    trapezoid_err(i,err)
+plt.plot(a,err)
+plt.ylabel("Error of trapezoid rule")
+
